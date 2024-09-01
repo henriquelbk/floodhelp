bc4aefdae2e32fef45d99fd45be5e96d1eecd1c8
henriquelbk <loubackhenrique123@gmail.com>
2024-09-01 19:53:57 -0300

    Basic structure created

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>  
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <header className="p-3 text-bg-dark">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center">
+          <h1 className="fw-bold text-light">Flood Help</h1>
+        </div>
+      </div>
+    </header>
   );
 }
